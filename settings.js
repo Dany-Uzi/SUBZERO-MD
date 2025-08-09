d3cc03700f8782fac506ba4ce390da941bdad802
Dany <danyuzi10@gmail.com>
2025-08-10 01:14:55 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
+  SESSION_ID: "SUBZERO-MD;;;vM1G0RwA#M1NZ0CzUFcflzoyuZE501tCYBFlNNJCaWfVoFCD9KgM", // Put Session Id Here Inside "*"
   OWNER_NUMBER: "263719647303", // Put Owner Number Here 
   PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
 };
